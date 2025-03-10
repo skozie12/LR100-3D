8f94c96f3880d7e244394fe05fd1f863ba3c0b88
skozie12 <de2smith@uwaterloo.ca>
2025-03-10 07:47:58 -0400

    Adjusting Launching Script

--- src/scripts/bash/start-dev.bat
+++ src/scripts/bash/start-dev.bat
@@ -1,3 +1,5 @@
 @echo off
 SET PATH=C:\Users\DuncanSmith_5zboqyf\Documents\node-v22.13.1-win-x64;%PATH%
-cmd /k "cd /d %~dp0 && echo Node.js environment ready! && echo."
+cd /d %~dp0
+cd ../../..
+cmd /k "echo Node.js environment ready! && echo."
