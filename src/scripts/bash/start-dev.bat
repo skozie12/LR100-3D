99e10a591cf1fd1ce47799e2ee7c8809173a6d0b
Duncan Smith <de2smith@uwaterloo.ca>
2025-03-11 15:25:54 -0400

    Repairing Node Install

--- src/scripts/bash/start-dev.bat
+++ src/scripts/bash/start-dev.bat
@@ -1,5 +1,5 @@
 @echo off
-SET PATH=C:\Users\DuncanSmith_5zboqyf\Documents\node-v22.13.1-win-x64;%PATH%
+SET PATH=C:\Users\Duncan Smith\LR100-3D\node_modules\node\node-modules\node-v22.13.1-win-x64;%PATH%
 cd /d %~dp0
 cd ../../..
 cmd /k "echo Node.js environment ready! && echo."
